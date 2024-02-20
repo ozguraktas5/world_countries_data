c2a3cc8e24af534e3972375abcbd70dd0b48227d
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-20 12:27:10 +0300

    20.02.2024

--- script.js
+++ script.js
@@ -9,13 +9,38 @@ document.addEventListener("DOMContentLoaded", function () {
     const sortedCountries = countries_data.sort(
       (a, b) => b.population - a.population
     );
-    const top10 = sortedCountries.slice(0, 10);
+    const top10Countries = sortedCountries.slice(0, 10);
 
-    top10.forEach((country) => {
+    top10Countries.forEach((country) => {
       const divItem = document.createElement("div");
       divItem.textContent = `${country.name} - ${country.population}`;
       container.appendChild(divItem);
     });
-    
+  });
+
+  languagesBtn.addEventListener("click", function () {
+    container.innerHTML = "";
+
+    const languagesCount = {};
+
+    countries_data.forEach((country) => {
+      country.languages.forEach((language) => {
+        if (languagesCount[language]) {
+          languagesCount[language]++;
+        } else {
+          languagesCount[language] = 1;
+        }
+      });
+    });
+
+    const sortedLanguages = Object.keys(languagesCount).sort((a, b) => languagesCount[b] - languagesCount[a]);
+
+    const top10Languages = sortedLanguages.slice(0, 10);
+
+    top10Languages.forEach((language) => {
+        const divItem = document.createElement("div");
+        divItem.textContent = `${language} - ${languagesCount[language]}`;
+        container.appendChild(divItem);
+    })
   });
 });
