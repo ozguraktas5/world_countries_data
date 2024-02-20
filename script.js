57bdd5917c8debd9838dab8282ae4fda4c5ee6fd
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-02-20 13:54:34 +0300

    20.02.2024

--- script.js
+++ script.js
@@ -106,27 +106,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
     top10Languages.forEach((language) => {
       const divItem = document.createElement("div");
+      divItem.className = "country-item";
 
       // Dil adını içeren div
       const languageDiv = document.createElement("div");
       languageDiv.textContent = language;
+      languageDiv.className = "country-name";
       divItem.appendChild(languageDiv);
-
-      // Dil sayısını içeren div
-      const countDiv = document.createElement("div");
-      countDiv.textContent = languagesCount[language];
-      divItem.appendChild(countDiv);
 
       // Barı oluştur
       const bar = document.createElement("div");
       bar.style.background = `linear-gradient(to right, orange ${
         (languagesCount[language] / languagesCount[top10Languages[0]]) * 100
       }%, transparent 0)`;
-      bar.style.height = "10px";
+      bar.className = "bar";
+      bar.style.height = "30px";
       bar.style.marginTop = "5px";
       divItem.appendChild(bar);
 
+      // Dil sayısını içeren div
+      const countDiv = document.createElement("div");
+      countDiv.textContent = languagesCount[language];
+      countDiv.className = "country-population";
+      divItem.appendChild(countDiv);
+
       container.appendChild(divItem);
     });
+
+    // Dil listesi oluşturulduktan sonra, div'leri hizalama işlemini gerçekleştir
+    alignDivs();
   });
+
+  // Div'leri hizalama fonksiyonu
+  function alignDivs() {
+    // Tüm country-item div'lerini seç
+    const countryItems = document.querySelectorAll(".country-item");
+
+    // Her bir country-item div için hizalama işlemini gerçekleştir
+    countryItems.forEach((item) => {
+      const children = item.children;
+
+      // Her bir çocuk öğeyi al ve hizala
+      for (let i = 0; i < children.length; i++) {
+        children[i].style.width = "30%";
+      }
+    });
+  }
 });
